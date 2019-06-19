e372aa8b491b403c1fd5938509b06eeedcefe824
Sean Xie <47431127+SeanJxie@users.noreply.github.com>
2019-06-18 20:23:53 -0400

    Add files via upload

--- BobTheTriceratops_Game.py
+++ BobTheTriceratops_Game.py
@@ -10,29 +10,49 @@ SC_HEIGHT = 600
 SC_TITLE = "Bob The Triceratops"
 
 # Constants
-PLAYER_X = 200
 PLAYER_LENGTH = 200
 PLAYER_HEIGHT = 100
 GRAVITY_CONSTANT = 1
 MAX_JUMP = 500
 FLOOR = 50
 
-# Variables
+PLATFORM_HEIGHT = 110
+PLATFORM_LENGTH = 300
+
+# Game variables --------------------------------------------------------------
+player_x = 200
 player_y = 100
 jumping = False
+
 # Used to avoid jumping while not on floor
 in_air = True
+
+# Used to represent x-value of the center of the screen
+# relative to the game environment
+screen_center = SC_WIDTH / 2
+
+# "Difficulty" of game
+game_speed = 1
+
+# Aesthetics variables --------------------------------------------------------
+# Amount and list of floor "tiles"
+floor_tiles = 8
+floor_x = [150 + 300 * k for k in range(floor_tiles)]
 
 
 # Constantly updating the functions
 def update(delta_time):
+    global screen_center, player_x
     ac.start_render()
     draw_floor()
-    draw_bob(PLAYER_X, player_y)
-
+    draw_bob(player_x, player_y)
     floor_boundary(FLOOR)
     gravity(GRAVITY_CONSTANT)
     jump(jumping)
+
+    # Moving the player and the viewport
+    viewport(screen_center)
+    move_with_screen()
 
 
 # PLAYER INTERACTION ----------------------------------------------------------
@@ -65,6 +85,22 @@ def jump(state):
             jumping = False
 
 
+# Moving the screen
+def viewport(viewport_x):
+    ac.set_viewport(
+        viewport_x - SC_WIDTH / 2,
+        viewport_x + SC_WIDTH / 2,
+        0,
+        SC_HEIGHT
+    )
+
+
+def move_with_screen():
+    global player_x, screen_center
+    player_x += game_speed
+    screen_center += game_speed
+
+
 # Everything that has to do with the "ground" in game
 def floor_boundary(y_bound):
     global player_y, in_air
@@ -88,18 +124,27 @@ def keypress(symbol, modifiers):
 def draw_bob(x, y):
     # Loading and drawing Bob
     bob_texture = ac.load_texture("Assets/Bob.png")
-    ac.draw_texture_rectangle(x, y, PLAYER_LENGTH,
-                              PLAYER_HEIGHT, bob_texture)
+    ac.draw_texture_rectangle(
+        x, y,
+        PLAYER_LENGTH, PLAYER_HEIGHT,
+        bob_texture
+    )
 
 
 def draw_floor():
     floor_texture = ac.load_texture("Assets/floor.png")
 
-    def fit_screen_floor(center_x):
-        for k in range(1, 5):
-            ac.draw_texture_rectangle(100, FLOOR,
-                                      300 + k * 300, 110, floor_texture)
-    fit_screen_floor(600)
+    for i in range(floor_tiles):
+        ac.draw_texture_rectangle(
+            floor_x[i], FLOOR,
+            PLATFORM_LENGTH, PLATFORM_HEIGHT,
+            floor_texture
+        )
+
+        # Check if a floor texture has moved off of the screen
+        # If so reset it to "in front" of the viewing area
+        if floor_x[i] <= screen_center - SC_WIDTH / 2 - PLATFORM_LENGTH / 2:
+            floor_x[i] += 2 * SC_WIDTH
 
 
 # All window related things
